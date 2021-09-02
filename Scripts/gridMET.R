dbe18ee9e5fa6c6934d5e44b3779180ed6ba192d
fitzLab-AL <mfitzpatrick@umces.edu>
2021-09-02 15:29:53 -0400

    progress

--- Scripts/gridMET.R
+++ Scripts/gridMET.R
@@ -1,17 +1,27 @@
 library(raster)
 
 years <- 1981:2000
-  
-for(i in 1:length(years){
-  year <- years[i]
-  ncs <- list.files(path="/Volumes/generalData/climateData/current/gridMET", pattern=".nc", full.names = T)
-  nc <- ncs[grep(year, ncs)]
-  climYear <- brick(nc[1])
-  dayDates <-  seq.Date(from=as.Date(paste0(year, "/1/1")), to=as.Date(paste0(year, "/12/31")), "day")
-  monthDates <- dayDates[grep(paste0(year, "-", "02"), dayDates)]
-  dateIndex <- which(dayDates %in% monthDates)
-  climMonth <- climYear[[dateIndex]]
-  climMonth[which(is.na(climMonth[]))] <- NA
-  ccc <- calc(climMonth, function(x){
-    mean(sort(x, decreasing=T)[1:10])
-  })
+tmps <- c("tmmn", "tmmx")[t]
+ncs <- list.files(path="/Volumes/generalData/climateData/current/gridMET", pattern=".nc", full.names = T)
+
+for(y in 1:length(years){
+  year <- years[y]
+  yearDates <-  seq.Date(from=as.Date(paste0(year, "/1/1")), to=as.Date(paste0(year, "/12/31")), "day")
+  for(t in 1:2){
+    tmp <- tmps[t]
+    nc <- ncs[grep(year, ncs)]
+    nc <- nc[grep(tmp, nc)]
+    climYear <- brick(nc)
+    for(m in 1:12){
+      if(m>10){
+        m <- as.character(m)
+      } else{
+        m <- as.character(paste0(0, m))
+      }
+      monthDates <- yearDates[grep(paste0(year, "-", m), yearDates)]
+      dateIndex <- which(yearDates %in% monthDates)
+      climMonth <- climYear[[dateIndex]]
+      climMonth[which(is.na(climMonth[]))] <- NA
+      extTempMonth <- calc(climMonth, function(x){
+        mean(sort(x, decreasing=T)[1:10])
+      })
