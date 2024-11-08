914f4b5a5352da3fa79dfec8cb6e138899f872cd
Fitzpatrick Lab <mfitzpatrick@umces.edu>
2024-11-08 14:04:17 -0500

    add terraclimate

--- Scripts/maxentModeling.R
+++ Scripts/maxentModeling.R
@@ -1,12 +1,61 @@
 library(dismo)
-library(mapdata)
+library(rnaturalearth)
+library(terra)
+library(sf)
+library(geodata)
+library(dplyr)
+library(ggplot2)
+library(ncdf4)
+library(QBMS)
 
-# medfly
-medfly <- gbif(genus="Ceratitis", 
-               species="capitata", 
-               geo=T, 
-               removeZeros=T)#,
-#ext=extent(c(-180, -30, 0, 90)))
+
+# medfly -----------------------------------------------------------------------
+# download worldclim monthly temperature data
+tavg <- worldclim_global(var='tavg', res=2.5, path="/home/mfitzpatrick/Projects/InvasionModels/")
+
+# medfly occurrence data from GBIF
+medfly <- sp_occurrence(genus='Ceratitis', species='capitata',
+                          download=TRUE, geom=TRUE, 
+                          removeZeros = TRUE) %>%
+  # select columns of interest
+  select(species, country, lon, lat, year, month, coordinateUncertaintyInMeters) %>%
+  # remove high uncertainty records
+  filter(coordinateUncertaintyInMeters < 5000) %>% # 2.5 arc-min = ~ 5km
+  # remove records before 2000
+  #filter(year >= 1970 & year <= 2000) %>%
+  # remove duplicates
+  distinct(lon, lat, .keep_all = TRUE) %>%
+  # convert to sf object
+  st_as_sf(coords = c("lon", "lat"), crs = 4326)
+
+# ggplot of records 
+ggplot() +
+  geom_sf(data = rnaturalearth::ne_countries(scale = "medium", returnclass = "sf")) +
+  geom_sf(data = medfly, aes(color = year)) +
+  theme_minimal()
+
+# function to extract temperature data from terraclimate
+
+get_terraclimate(
+  -1.59,
+  -77.71,
+  from = "1958-01-01",
+  to = "1960-01-01",
+  clim_vars = "tmax",
+  month_mask = 1,
+  offline = FALSE)
+
+
+# read in the full period of record using aggregated files
+
+data <- as.numeric(ncvar_get(nc, varid = var, start = start, count))
+
+
+#remove spatial duplicates:  
+mf_cells <- cellFromXY(tavg, st_coordinates(medfly)) #identify cells from raster for all occurrences
+mf_dup <- duplicated(mf_cells) #which cells have multiple occurrences (ie spatial duplicates)
+medfly <- medfly[!mf_dup,] #remove spatial duplicates
+
 
 #medfly <- subset(medfly, country %in% c("United States", "Canada", "Mexico"))
 
