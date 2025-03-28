d81d3f82593162328e730038e67eccce26768c8b
Fitzpatrick Lab <mfitzpatrick@umces.edu>
2025-03-28 16:37:45 -0400

    Add data desc, remove dups

--- Scripts/compare2SDMs.R
+++ Scripts/compare2SDMs.R
@@ -4,6 +4,19 @@ library(sf)
 library(dplyr)
 
 # load brte cover data from Larson & Tuor (2021) Remote Sensing
+# Downs et al. [15] compiled over 24,000 field vegetation measurements in the 
+# historic range of sage-grouse that were collected on multiple unrelated field 
+# campaigns between 2001 to 2014 (Figure 1). Of these observations, 6418 are 
+# deemed useful based on geographic accuracy and overlap with the study area, 
+# completeness, and rigor of collection methods. This was further reduced to 5973 
+# after removing observations that had incomplete satellite data and were less 60-m 
+# apart (i.e., the distance of at least two pixels). Nearest-neighbor spacing of 
+# field observations ranged from 61 to 79,421-m with a mean distance of 1837-m. 
+# Most of the excluded observations are from the U.S. Department of Agriculture 
+# (USDA) Forest Inventory and Analysis program, which does not provide the true 
+# geographic location of the publicly available version of its data. All field 
+# data were collected from transects ranging from 25 m to 100 m in length using 
+# point intercept or standardized plot frame techniques.
 vegPlot <- read.csv("/Users/mfitzpatrick/Desktop/fieryfuture/clean_jan_field_data.csv")
 
 vegPlot <- vegPlot %>%
@@ -15,24 +28,25 @@ vegPlot <- vegPlot %>%
   st_as_sf(coords = c("longitude_30m", "latitude_30m"), crs = 4326)
 
 # download GBIF records for brte - takes several minutes to run
-bromusGBIF <- sp_occurrence(genus = "Bromus",
-                            species = "tectorum",
-                            download=T,
-                            geo=T,
-                            ext=ext(c(-130, -95, 0, 90)), #western NA
-                            removeZeros = T) %>%
-  # select columns of interest
-  select(species, country, lon, lat, year, coordinateUncertaintyInMeters) %>%
-  #remove records outside of Australia
-  filter(country %in% c("United States", "Canada", "Mexico")) %>%
-  # remove high uncertainty records
-  filter(coordinateUncertaintyInMeters <= 1000) %>%
-  # remove records before 2000
-  filter(year >= 2000) %>%
-  # remove duplicates
-  distinct(lon, lat, .keep_all = TRUE) %>%
-  # convert to sf object
-  st_as_sf(coords = c("lon", "lat"), crs = 4326)
+### DATA USED IN THE PAPER ARE HERE: https://doi.org/10.15468/dl.fk2zuc
+# bromusGBIF <- sp_occurrence(genus = "Bromus",
+#                             species = "tectorum",
+#                             download=T,
+#                             geo=T,
+#                             ext=ext(c(-130, -95, 0, 90)), #western NA
+#                             removeZeros = T) %>%
+#   # select columns of interest
+#   select(species, country, lon, lat, year, coordinateUncertaintyInMeters) %>%
+#   #remove records outside of Australia
+#   filter(country %in% c("United States", "Canada", "Mexico")) %>%
+#   # remove high uncertainty records
+#   filter(coordinateUncertaintyInMeters <= 1000) %>%
+#   # remove records before 2000
+#   filter(year >= 2000) %>%
+#   # remove duplicates
+#   distinct(lon, lat, .keep_all = TRUE) %>%
+#   # convert to sf object
+#   st_as_sf(coords = c("lon", "lat"), crs = 4326)
 
 # load prediction rasters
 # A typical SDM from McMahon et al. (2021) Diversity & Distributions
@@ -64,6 +78,14 @@ write.csv(bromusStackGBIF, "/Users/mfitzpatrick/code/InvasionModels/Output/bromu
 bromusStackVegPlot <- extract(bromusStack, vegPlot, cells=F, xy=T, ID=F)
 head(bromusStackVegPlot)
 bromusStackVegPlot <- cbind(bromusStackVegPlot, bromusCover=vegPlot$brte_cov)
+
+# use group_by to calculate mean values of bromus cover for each unique x-y pair
+# using .groups argument to avoid warning message
+bromusStackVegPlot <- bromusStackVegPlot %>%
+  group_by(bromus_lambdaPred.v2, bromus_SDM, bromus_remoteSensing, x, y,) %>%
+  summarise(bromusCover=mean(bromusCover, na.rm=T)) %>%
+  data.frame()
+
 write.csv(bromusStackVegPlot, "/Users/mfitzpatrick/code/InvasionModels/Output/bromusStackVegPlot.csv")
 
 par(mfrow=c(3,2))
