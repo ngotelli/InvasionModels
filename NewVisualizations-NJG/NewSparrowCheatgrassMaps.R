af6238172cb2740dd036b71cefa6be125f864122
ngotelli <ngotelli@uvm.edu>
2025-04-14 12:55:20 -0400

    corrected data frame from Matt

--- NewVisualizations-NJG/NewSparrowCheatgrassMaps.R
+++ NewVisualizations-NJG/NewSparrowCheatgrassMaps.R
@@ -12,7 +12,7 @@ library(ggplot2)
 #                header=TRUE,sep=",")
 # saveRDS(z,"HouseSparrow.rds")
 
-z <- readRDS("NewVisualizations-NJG/HouseSparrow.rds")
+z <- readRDS("Output/HouseSparrowGrowthPredictions_at_eBird_points.rds")
 ggplot(data=z,aes(x=x, y=y)) +
   geom_raster(aes(fill = layer)) +
   coord_fixed() +  # Maintain aspect ratio
@@ -21,3 +21,17 @@ ggplot(data=z,aes(x=x, y=y)) +
   labs(x="",y="") +
    theme_grey(base_size=18) + 
   scale_fill_gradientn(colors = hcl.colors(10, "Viridis"))
+
+
+
+z$status <- "positive"
+z$status[z$layer<=0] <- "negative"
+ggplot(data=z,aes(x=x, y=y)) +
+  geom_raster(aes(fill = status)) +
+  coord_fixed() +  # Maintain aspect ratio
+  xlim(-130,-65) +
+  ylim(25,55) +
+  labs(x="",y="") +
+  theme_grey(base_size=18) + 
+  #scale_fill_gradientn(colors = hcl.colors(10, "Viridis"))
+  scale_fill_discrete(c("coral","cornflower blue"))
