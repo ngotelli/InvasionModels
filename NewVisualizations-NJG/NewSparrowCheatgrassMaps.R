d5b936c0cd15dfec75bddd2ac4a4ba4fbb24d906
Fitzpatrick Lab <mfitzpatrick@umces.edu>
2025-05-07 10:50:15 -0400

    add USA polygons

--- NewVisualizations-NJG/NewSparrowCheatgrassMaps.R
+++ NewVisualizations-NJG/NewSparrowCheatgrassMaps.R
@@ -5,6 +5,9 @@
 # NJG
 
 library(ggplot2)
+library(sf) # for spatial data
+library(geodata) # admin boundaries
+library(spatialEco) # dissolve polygons
 
 # This code ran outside of the repo to create the rds file:
 
@@ -13,7 +16,19 @@ library(ggplot2)
 # saveRDS(z,"HouseSparrow.rds")
 
 # house sparrow plots
+#z <- readRDS("/Users/mfitzpatrick/code/InvasionModels/Output/HouseSparrowGrowthPredictions_at_eBird_points.rds")
 z <- readRDS("Output/HouseSparrowGrowthPredictions_at_eBird_points.rds")
+
+# boundary of USA + states from geodata package
+usaStates <- geodata::gadm("USA", level = 1, path=getwd())
+# remove Alaska and Hawaii
+usaStates <- usaStates[usaStates$NAME_1 != "Hawaii", ]
+usaStates <- usaStates[usaStates$NAME_1 != "Alaska", ]
+# convert to sf object
+usaStates <- sf::st_as_sf(usaStates)
+# dissolve the states into a single polygon
+usa <- spatialEco::sf_dissolve(usaStates, "COUNTRY")
+
 ggplot(data=z,aes(x=x, y=y)) +
   geom_raster(aes(fill = layer)) +
   coord_fixed() +  # Maintain aspect ratio
@@ -23,7 +38,16 @@ ggplot(data=z,aes(x=x, y=y)) +
    theme_grey(base_size=18) + 
   scale_fill_gradientn(colors = hcl.colors(10, "Viridis"))
 
-
+# version that adds USA boundary
+ggplot() +
+  geom_raster(data = z, aes(x = x, y = y, fill = layer)) +
+  geom_sf(data = usa, color = "black", fill = NA) + # add USA polygon
+  coord_sf() +  # seems to be required is plotting an sf object...
+  xlim(-130,-65) +
+  ylim(25,55) +
+  labs(x="",y="") +
+  theme_grey(base_size=18) + 
+  scale_fill_gradientn(colors = hcl.colors(10, "Viridis"))
 
 z$status <- "positive"
 z$status[z$layer<=0] <- "negative"
@@ -35,21 +59,34 @@ ggplot(data=z,aes(x=x, y=y)) +
   labs(x="",y="") +
   theme_grey(base_size=18) + 
   #scale_fill_gradientn(colors = hcl.colors(10, "Viridis"))
-  scale_fill_discrete(c("coral","cornflower blue"))
+  scale_fill_discrete(c("coral","cornflowerblue"))
 
 # Run outside of repo to create data object
  # z<- read.table(file="../../UTF-8BromusStackVegPlot.csv",
  #                header=TRUE,sep=",")
  # saveRDS(z,"DataObjects/BromusStackVegPlot.rds")
+#z <- readRDS("/Users/mfitzpatrick/code/InvasionModels/DataObjects/BromusStackVegPlot.rds")
 z <- readRDS("DataObjects/BromusStackVegPlot.rds")
 z <- na.omit(z)
 z$status <- "positive"
 z$status[z$bromus_lambdaPred.v2<=1.0] <- "negative"
  # z <-z[z$bromus_lambdaPred.v2>0,]
-ggplot(data=z,aes(x=x, y=y,color=status)) +
-  geom_point(size=0.65) +
-   coord_fixed() +  # Maintain aspect ratio
+
+# version that adds USA states boundaries
+# can't seem to get teh point colors to work...
+ggplot() +
+  geom_sf(data = usaStates, color = "black", fill = NA) +
+  geom_point(data = z, aes(x = x, y = y, fill = status), size=0.65) +
+  xlim(-125, -100) +
+  ylim(25,50) +
   labs(x="",y="") +
   theme_grey(base_size=16) + 
-  
-  scale_fill_discrete(c("coral","cornflower blue"))
+  scale_fill_discrete(c("coral","cornflowerblue"))
+
+ggplot(data=z,aes(x=x, y=y,color=status)) +
+  geom_point(size=0.65) +
+  coord_fixed() +  # Maintain aspect ratio
+  labs(x="",y="") +
+  theme_grey(base_size=16) + 
+  scale_fill_discrete(c("coral","cornflowerblue"))
+
